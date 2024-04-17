0894eb4c0ba0642c0dd08d0cb8d5d5e31595e6cb
kerabite <10791585+kerabite@users.noreply.github.com>
2024-04-17 12:40:25 -0500

    Refactored AdjacencyMatrix to AdjacencyList, added Equality functions

--- Runtime/Graph.cs
+++ Runtime/Graph.cs
@@ -5,51 +5,99 @@ using JetBrains.Annotations;
 // maybe put stuff like "known isomorphisms" or "clique number" stuff in the future.
 public class Graph<T>
 {
-    public AdjacencyMatrix<T> AdjacencyMatrix { get; }
-    public HashSet<T> VertexSet => AdjacencyMatrix.VertexSet;
+    public AdjacencyList<T> AdjacencyList { get; }
+    public HashSet<T> VertexSet => AdjacencyList.VertexSet;
 
-    public Graph(AdjacencyMatrix<T> adjacencyMatrix)
+    public Graph(AdjacencyList<T> adjacencyList)
     {
-        AdjacencyMatrix = adjacencyMatrix;
+        AdjacencyList = adjacencyList;
     }
 
     public Graph(HashSet<T> vertexSet, IAdjacencyFunction<T> adjacencyFunction)
     {
-        AdjacencyMatrix = new AdjacencyMatrix<T>(vertexSet, adjacencyFunction);
+        AdjacencyList = new AdjacencyList<T>(vertexSet, adjacencyFunction);
     }
 
     public bool CheckAdjacent(T v, T w)
     {
-        return AdjacencyMatrix.CheckAdjacent(v, w);
+        return AdjacencyList.CheckAdjacent(v, w);
+    }
+
+    // untested
+    public override bool Equals(object obj)
+    {
+        return obj is Graph<T> graph && Equals(graph);
+    }
+
+    // untested
+    private bool Equals(Graph<T> other)
+    {
+        return Equals(AdjacencyList, other.AdjacencyList);
+    }
+
+    // TODO: make Equals with Isomorphism
+
+    public override int GetHashCode()
+    {
+        return (AdjacencyList != null ? AdjacencyList.GetHashCode() : 0);
     }
 }
 
 // try to consolidate into graph.
-public class AdjacencyMatrix<T>
+public class AdjacencyList<T>
 {
-    [NotNull] private readonly Dictionary<T, Dictionary<T, bool>> _matrix = new();
+    [NotNull] private readonly Dictionary<T, HashSet<T>> _matrix = new();
     public HashSet<T> VertexSet => new HashSet<T>(_matrix.Keys);
-
-
-    public AdjacencyMatrix(Dictionary<T, Dictionary<T, bool>> matrix)
+    
+    public AdjacencyList(Dictionary<T, HashSet<T>> matrix)
     {
-        // warning! should validate that all (v,w) pairs actually exist in the future
         _matrix = matrix;
     }
-    public AdjacencyMatrix(HashSet<T> vertexSet, IAdjacencyFunction<T> adjacencyFunction)
+    public AdjacencyList(HashSet<T> vertexSet, IAdjacencyFunction<T> adjacencyFunction)
     {
         foreach (var v in vertexSet)
         {
-            _matrix[v] = new Dictionary<T, bool>();
+            _matrix[v] = new HashSet<T>();
             foreach (var w in vertexSet)
             {
-                _matrix[v][w] = adjacencyFunction.CheckAdjacent(v, w);
+                if (adjacencyFunction.CheckAdjacent(v, w))
+                {
+                    _matrix[v].Add(w);
+                }
             }
         }
     }
 
     public bool CheckAdjacent(T v, T w)
     {
-        return _matrix[v][w];
+        if (!_matrix.ContainsKey(v))
+        {
+            // potentially throw error in the future
+            return false;
+        }
+        return _matrix[v].Contains(w);
+    }
+
+    // untested
+    public override bool Equals(object obj)
+    {
+        return obj is AdjacencyList<T> adjacencyList && Equals(adjacencyList);
+    }
+
+    // untested
+    private bool Equals(AdjacencyList<T> adjacencyList)
+    {
+        if (adjacencyList.VertexSet.Count != this.VertexSet.Count) return false;
+        foreach (var (v, neighborhood) in _matrix)
+        {
+            if (neighborhood.Any(w => !adjacencyList.CheckAdjacent(v, w)))
+            {
+                return false;
+            }
+            // TODO: didn't check nonadjacent vertices are still nonadjacent
+            // TODO: move this to an IdentityIsomorphism
+        }
+
+        return true;
     }
 }
